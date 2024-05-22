9a1ca5a1a07d529e6ac34a05e6c68fe1bb7fa16f
Chris TF <81719250+chris1234345@users.noreply.github.com>
2024-05-22 19:12:35 +0300

    Begin workin on backend

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -21,6 +21,7 @@ import CustomInput from './CustomInput'
 import { authformSchema } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
 import { useRouter } from 'next/navigation'
+import { signIn, signUp } from '@/lib/actions/user.actions'
 
 
 
@@ -50,17 +51,17 @@ const AuthForm = ({type}: {type:string}) => {
       // Sign up with Appwrite & create plaid token
 
       if(type === 'sign-up'){
-        // const newUser = await signUp(data)
+        const newUser = await signUp(data)
 
-        // setUser(newUser);
+        setUser(newUser);
       }
       if(type === 'sign-in'){
-        // const response = await signIn({
-        //   email: data.email,
-        //   password: data.password
-        // })
+        const response = await signIn({
+          email: data.email,
+          password: data.password
+        })
 
-        // if(response) router.push('/')
+        if(response) router.push('/')
       }
 
     } catch(error) {
---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,18 @@
+"use sever"
+
+export const signIn = async () => {
+    try {
+        // Mutation / Database / make fetch
+    } catch(error) {
+        console.log(error, 'Error')
+    }
+}
+
+export const signUp = async (userData: SignUpParams) => {
+    try {
+
+        // create a use account
+    } catch(error) {
+        console.log(error, 'Error')
+    }
+}
